1dfca0330488764f0c31c540991f374c95fc7f60
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-26 12:28:47 -0600

    Refactor: #format_facility and #create_facilities

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,32 +1,31 @@
 class FacilityFactory
   def create_facilities(state_raw_data)
-    created_facilities = state_raw_data.map do |facility| 
+    state_raw_data.map do |facility|
       facility_details = format_facility(facility)
-        Facility.new(
-          {
-            name:      facility_details[:name],
-            address:   facility_details[:address],
-            phone:     facility_details[:phone]
-          }
-        )
-      end
-    created_facilities
+      Facility.new(
+        {
+          name:      facility_details[:name],
+          address:   facility_details[:address],
+          phone:     facility_details[:phone]
+        }
+      )
+    end
   end
 
   def format_facility(data)
-    data = {
-        name:          format_name(data),
-        address:       format_address(data),
-        phone:         format_phone(data)
-      }
+    {
+      name:          format_name(data),
+      address:       format_address(data),
+      phone:         format_phone(data)
+    }
   end
 
   def format_name(data)
     if data[:state] == "NY"
       data[:office_name]
     elsif data[:state] == "MO"
-      data[:name].rstrip 
-    elsif data[:website].include?("ODOT")  
+      data[:name].rstrip
+    elsif data[:website].include?("ODOT")
       data[:title]
     end
   end
@@ -36,7 +35,7 @@ class FacilityFactory
       "#{data[:street_address_line_1]}, #{data[:city]}, #{data[:state]} #{data[:zip_code]}"
     elsif data[:state] == "MO"
       "#{data[:address1]}, #{data[:city]}, #{data[:state]} #{data[:zipcode]}"
-    elsif data[:website].include?("ODOT")  
+    elsif data[:website].include?("ODOT")
       parsed = JSON.parse(data[:location_1][:human_address], {symbolize_names: true})
       "#{parsed[:address]}, #{parsed[:city]}, #{parsed[:state]} #{parsed[:zip]}"
     end
@@ -47,7 +46,7 @@ class FacilityFactory
       data[:public_phone_number]
     elsif data[:state] == "MO"
       data[:phone]
-    elsif data[:website].include?("ODOT")  
+    elsif data[:website].include?("ODOT")
       data[:phone_number]
     end
   end
