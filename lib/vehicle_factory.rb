182344cc484cbdf855bf40d23bb2f3c7fd319286
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-26 12:25:19 -0600

    Refactor: VehicleFactory

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,12 +1,12 @@
 class VehicleFactory
   def initialize
-    
+
   end
 
   def create_vehicles(registrations)
     created_vehicles = registrations.map do |car|
       Vehicle.new(
-        { 
+        {
           vin:    car[:vin_1_10],
           year:   car[:model_year],
           make:   car[:make],
@@ -15,6 +15,5 @@ class VehicleFactory
         }
       )
     end
-    created_vehicles
   end
 end
